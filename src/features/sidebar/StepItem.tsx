825ecbf26e5c03f515939400cf3fbe880964d9e6
lelouch187 <lelouch187@mail.ru>
2023-06-15 12:25:03 +0600

    bag fix

--- src/features/sidebar/StepItem.tsx
+++ src/features/sidebar/StepItem.tsx
@@ -8,12 +8,12 @@ interface IStepItem {
 }
 
 const StepItem: FC<IStepItem> = ({ text, numberStep, currentStep }) => {
-  const step = numberStep>4?4:numberStep;
+  const step = currentStep>4?4:numberStep;
   return (
     <div className="step__wrapper">
       <div className={step===currentStep?"step__number active":"step__number"}>{numberStep}</div>
       <div className="step__info">
-        <p className="step__subtitle">step {currentStep}</p>
+        <p className="step__subtitle">step {numberStep}</p>
         <h3 className="step__title">{text}</h3>
       </div>
     </div>
